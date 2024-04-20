eea93b22fb61ec20f34fc37c4cafc5c60dddc467
conniely <conniely04@gmail.com>
2024-04-20 03:15:10 -0700

    server changes

--- server/server.js
+++ server/server.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+// app.use(cors());
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from the server!" });
