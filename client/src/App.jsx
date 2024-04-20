cb6219ad36fab395d9d9bc15764e0d2a481aeee0
conniely <conniely04@gmail.com>
2024-04-20 01:36:52 -0700

    added browser router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import Landing from "./Pages/Landing";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Landing />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          {/* Add other routes here */}
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
