0e7d3c6b2e5dd7d58cd3548e97472ad4cc5bf198
lyeric2022 <111820150+lyeric2022@users.noreply.github.com>
2024-04-20 12:34:46 -0700

    :D

--- client/src/Pages/CreateAccount.jsx
+++ client/src/Pages/CreateAccount.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import React, { useState } from "react";
 // import './CreateAccount.css';  // Assume you have styles defined in CreateAccount.css
 
 function CreateAccount() {
